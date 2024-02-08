651940a910ccc9e3004f16e81b899271c6d254db
Dary Cabrera <cabreradary.gmail.com>
2024-02-08 15:17:35 -0700

    Day 5

---
+++ solutions/Day05/Part2.py
@@ -0,0 +1,45 @@
+# Answer LVZPSTTCZ
+import sys
+
+with open(sys.argv[1], "r", encoding="ascii") as fl:
+    rows = []
+    for line in fl:
+        if line.isspace():
+            break
+        else:
+            rows.append(line)
+
+    zeKeys = rows.pop().strip().split()
+    column = 1
+    stacks = dict()
+    for key in zeKeys:
+        stack = []
+        for row in reversed(rows):
+            cell = row[column]
+            if not cell.isspace():
+                stack.append(cell)
+        column += 4
+        stacks[key] = stack
+
+    for line in fl:
+        tokens = line.split()
+        crate_moves = int(tokens[1])
+        src_stack = tokens[3]
+        dest_stack = tokens[5]
+        temp_stack = []
+        moves = crate_moves
+        while moves > 0:
+            crate = stacks[src_stack].pop()
+            temp_stack.append(crate)
+            moves -= 1
+        moves = crate_moves
+        while moves > 0:
+            crate = temp_stack.pop()
+            stacks[dest_stack].append(crate)
+            moves -= 1
+
+    topCrates = []
+    for stack in stacks.values():
+        topCrates.append(stack[-1])
+
+    print("".join(topCrates))
